9972d95fd52144bc7344263a6be3b0c985f0d9a2
Nina Samykova <118392730+Nnisik@users.noreply.github.com>
2024-01-01 19:07:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// alert("Hello World");
+function clearCalendarArea(parentDiv) {
+    parentDiv.innerHTML = '';
+}
+
+function drawCalendar(parentDiv, startSum, addSum) {
+    let finalSum = 0;
+    let currentAdd = startSum;
+
+    for (let i = 0; i < 52; i++) {
+        // creating week element
+        let childDiv = document.createElement("div");
+        childDiv.className = "week";
+        // creating week element's elements
+        // week number
+        let childDivNumber = document.createElement("div");
+        childDivNumber.className = "week-top";
+        childDivNumber.innerHTML = i + 1;
+        // sum element
+        let childDivSum = document.createElement("div");
+        childDivSum.className = "week-main";
+        childDivSum.innerHTML = currentAdd;
+        // combining elements
+        childDiv.appendChild(childDivNumber);
+        childDiv.appendChild(childDivSum);
+        parentDiv.appendChild(childDiv)
+        // changing varuables
+        finalSum += currentAdd;
+        currentAdd += addSum;
+    }
+
+    document.getElementById("final-sum").innerHTML = "Final Sum: " + finalSum;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("calculate-calendar").onclick = () => {
+        let startSum = parseInt(document.getElementById("start-sum").value);
+        let weekAdd = parseInt(document.getElementById("week-add").value);
+
+        const calendarDiv = document.getElementById("calculator");
+        
+        clearCalendarArea(calendarDiv);
+        drawCalendar(calendarDiv, startSum, weekAdd);
+    }
+});
